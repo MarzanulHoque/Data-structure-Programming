b7ab5162bf5e3ec68e42e31b1f7151f1ad654edd
Susmoy Barman <susmoy11223300@gmail.com>
2018-04-01 01:24:43 +0600

    Multiplication of two matrix using array

---
+++ matrix_multiplication.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int m,n,p,arr1[100][100],arr2[100][100],arr3[100][100];
+
+void multiplication(int m,int p,int n)
+{
+	for(int i=0;i<m;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			arr3[i][j] = 0;
+			for(int k=0;k<p;k++)
+			{
+				arr3[i][j] += arr1[i][k] * arr2[k][j];
+			}
+		}
+	}
+}
+void print()
+{
+	cout<<endl;
+	for(int i=0;i<m;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			cout<<arr3[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	cout<<endl;
+}
+int main()
+{
+	cin>>m>>p>>n;
+	for(int i=0;i<m;i++)
+	{
+		for(int j=0;j<p;j++)
+		{
+			cin>>arr1[i][j];
+		}
+	}
+	for(int i=0;i<p;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			cin>>arr2[i][j];
+		}
+	}
+	multiplication(m,p,n);
+	print();
+}
